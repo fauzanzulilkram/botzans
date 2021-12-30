600bd0f1d56cb86f2a8f2156cf0214b6c1b091e9
fauzanzulilkram <95626939+fauzanzulilkram@users.noreply.github.com>
2021-12-30 22:34:43 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/fauzanzulilkram/botzans/main/public/tfjs_model/model.jsonn';
+        const path = 'https://raw.githubusercontent.com/fauzanzulilkram/botzans/main/public/tfjs_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
